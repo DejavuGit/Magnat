b5162c77a090151df7c15cf9b5e2893d9316faf9
Dejavu <Dejavugit@gmail.com>
2022-09-07 00:16:10 +0300

    end today

--- app/js/main.js
+++ app/js/main.js
@@ -19,3 +19,23 @@ let swiper = new Swiper(".mySwiper", {
  },
 });
 
+let moreInfoSwiper = new Swiper(".more-info__swiper", {
+
+  // slidesPerView: "auto",
+        // centeredSlides: true,
+        spaceBetween: 30,
+  slidesPerView: 4,
+  // loop: true,
+  // speed: 1500,
+  // slidesPerView: "auto",
+  centeredSlides: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   type: "fraction",
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+ });
+
