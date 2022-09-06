dcced35079e7b461c7592a5bc711b431cc0f54ee
Dejavu <Dejavugit@gmail.com>
2022-09-06 23:10:51 +0300

    add section buy-most

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,18 @@ dropdown.forEach(drop =>{
   this.classList.toggle('dropdown--active');
  })
 })
+
+let swiper = new Swiper(".mySwiper", {
+ slidesPerView: 1,
+ loop: true,
+ speed: 1500,
+ pagination: {
+   el: ".swiper-pagination",
+   type: "fraction",
+ },
+ navigation: {
+   nextEl: ".swiper-button-next",
+   prevEl: ".swiper-button-prev",
+ },
+});
+
